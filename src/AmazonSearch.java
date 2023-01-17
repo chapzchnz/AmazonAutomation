469f02ccdd6cc5df0a901a8f24a3f862c8e5795f
chamalee samarawijaya <48786870+chapzchnz@users.noreply.github.com>
2023-01-18 00:14:54 +0200

    Initial commit

--- src/AmazonSearch.java
+++ src/AmazonSearch.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class AmazonSearch {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonSearch {
+    public static void main(String[] args) {
+        // Create a new instance of the Chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        // Go to the Amazon website
+        driver.get("https://www.amazon.com/");
+
+        // Find the search box element and enter a search term
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Selenium");
+
+        // Find the search button element and click it
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+        // Find the delivery to  latvia  and click it by xpath
+        driver.findElement(By.xpath("//*[@id=\"nav-global-location-slot\"]")).click();
+
+
+
+
+    }
 }
+
+
+
+
+
+
+
